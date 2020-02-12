0a04d648d0ea5ded06b6cfaf985fc7180b54a0ca
Supavet Amornruksakul <Supavet.a@ku.th>
2020-02-12 11:32:02 +0700

    add week2.c basic, printf, gets, fgets, atoi, atof, atol, if else , Loop

---
+++ week2.c
@@ -0,0 +1,59 @@
+// #include<stdio.h>
+// #include<stdlib.h>
+// char name[20];
+// char number[5];
+// int main(){
+//     printf("%s\n",name);
+//     int i;
+//     scanf("%s %d",name,&i);
+//     printf("Enter name : ");
+//     gets(name);
+//     fgets(name, 20, stdin);
+//     printf("name = %s\n",name);
+//     char number[5];
+//     fgets(number, 5, stdin);
+//     int i = atoi(number);
+//     printf("number = %s, i = %d\n",number,i);
+
+//     char a = getchar();
+//     getchar();
+//     char b = getchar();
+
+//     long l = atol(number);
+//     double d = atof(number);
+
+//     puts(number);
+//     putchar(100); putchar('a');
+
+//     int found = 0 ;
+//     float isExit = 0.0;
+//     char isOnline= '\0';
+
+//     fgets(number, 5, stdin);
+//     int i = atoi(number);
+
+//     if(i >= 0 && i <= 10){
+//         printf("if = %d", i >= 0 && i <= 10);
+//     }else if(i <= 50 || !found){
+//         printf("else if");
+//     }else{
+//         printf("else");
+//     }
+
+//     fgets(number, 5, stdin);
+//     int i = atoi(number);
+//     int count = 0;
+//     while (count < i){
+//         printf("%d\n",count);
+//         count++;
+//     }
+//     for(int j = 0 ; j < 1 ; j++){
+//         printf("%d\n",j);
+//     }
+
+    // while(1){        //Infinite loop
+
+    // }
+
+
+// }
